44328ce84b8a470fc125ce777d10775fbf45d7d0
Hina <15116318341@163.com>
2023-07-15 20:01:42 -0700

    first commit

---
+++ server/models/Post.js
@@ -0,0 +1,37 @@
+import mongoose from "mongoose";
+
+const postSchema = mongoose.Schema(
+  {
+    userId: {
+      type: String,
+      required: true,
+    },
+    firstName: {
+      type: String,
+      required: true,
+    },
+    lastName: {
+      type: String,
+      required: true,
+    },
+    location: String,
+    description: String,
+    picturePath: String,
+    userPicturePath: String,
+    likes: {
+      type: Map,
+      of: Boolean,
+      // a Map that stores userId as the key 
+      //and a boolean value indicating whether the post is liked or not
+    },
+    comments: {
+      type: Array,
+      default: [],
+    },
+  },
+  { timestamps: true }
+);
+
+const Post = mongoose.model("Post", postSchema);
+
+export default Post;
